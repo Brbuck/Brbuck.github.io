d72ab7e8a37b0029f6ba162595e0a77a9f02c2d1
Brbuck <62665590+Brbuck@users.noreply.github.com>
2021-03-25 08:50:03 -0300

    Alterações no JS

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,30 @@ const menu = document.querySelector('.nav-ul')
 toggle.addEventListener('click', () =>{
     menu.classList.toggle('show')
 })
+
+
+
+const news = document.querySelector('.news')
+const menuNews = document.querySelector('.nav-news')
+
+news.addEventListener('click', () =>{
+    menuNews.classList.toggle('shownews')
+})
+
+
+
+const cotacao = document.querySelector('.cotacao')
+const menucota = document.querySelector('.nav-cotacao')
+
+cotacao.addEventListener('click', () =>{
+    menucota.classList.toggle('showcotacao')
+    
+})
+
+
+const ferramenta = document.querySelector('.ferramenta')
+const menuferramenta = document.querySelector('.nav-ferramenta')
+
+ferramenta.addEventListener('click', () =>{
+    menuferramenta.classList.toggle('showferramenta')
+})
